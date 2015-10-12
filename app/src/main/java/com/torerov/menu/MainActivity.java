29cf1683bb6dece247e0a08474d52d4d089d0377
choijaewoong <jwchoi0129@gmail.com>
2015-10-12 12:14:04 +0900

    ActionMode / Popup Menu 추가

--- app/src/main/java/com/torerov/menu/MainActivity.java
+++ app/src/main/java/com/torerov/menu/MainActivity.java
@@ -1,13 +1,18 @@
 package com.torerov.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,10 +22,75 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        registerForContextMenu(findViewById(R.id.text_message)); //롱클릭으로 컨텍스트 메뉴 등록
+//        registerForContextMenu(findViewById(R.id.text_message)); //롱클릭으로 컨텍스트 메뉴 등록
         registerForContextMenu(findViewById(R.id.imageView));
+        Button btn = (Button)findViewById(R.id.btn_action);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showActionMode();
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_popup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, v);
+                popup.inflate(R.menu.menu_action_mode);
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu_mode_item1 :
+                            case R.id.menu_mode_item2 :
+                                Toast.makeText(MainActivity.this, "Popup Item Click : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+                popup.show();
+            }
+        });
+
     }
+
+    private void showActionMode() {
+        ActionMode mode = startSupportActionMode(new ActionMode.Callback() {
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                getMenuInflater().inflate(R.menu.menu_action_mode, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_mode_item1 :
+                    case R.id.menu_mode_item2 :
+                        Toast.makeText(MainActivity.this, "Mode Item Click : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        mode.finish();
+                        return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+
+            }
+        });
+        mode.setTitle("Action Mode Menu");
+    }
+
     EditText keywordView;
+    ShareActionProvider mActionProvider;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -49,7 +119,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        item = menu.findItem(R.id.menu_item3);
+        mActionProvider = (ShareActionProvider)MenuItemCompat.getActionProvider(item);
+        doShareAction();
         return true;
+    }
+
+    private void doShareAction(){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/+");
+        mActionProvider.setShareIntent(intent);
     }
 
     @Override
@@ -58,16 +137,16 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menu_item1 :
                 Toast.makeText(this, "Menu 1", Toast.LENGTH_SHORT).show();
                 return true;
-            case R.id.menu_item2 :
-                Toast.makeText(this, "Menu 2", Toast.LENGTH_SHORT).show();
-                return true;
+//            case R.id.menu_item2 :
+//                Toast.makeText(this, "Menu 2", Toast.LENGTH_SHORT).show();
+//                return true;
             case R.id.menu_item3 :
                 Toast.makeText(this, "Menu 3", Toast.LENGTH_SHORT).show();
                 return true;
-            case R.id.sub_item1 :
-            case R.id.sub_item2 :
-                Toast.makeText(this, "Sub Menu : " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                return true;
+//            case R.id.sub_item1 :
+//            case R.id.sub_item2 :
+//                Toast.makeText(this, "Sub Menu : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+//                return true;
         }
         return super.onOptionsItemSelected(item);
     }
